c4120e68716d640d14d116082bee2f6b7b913825
mateusznasewicz <mateusznasewicz@gmail.com>
2025-03-20 13:04:24 +0100

    init

---
+++ recipe-manager-backend/src/main/java/pl/mateusz/recipemanagerbackend/RecipeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.mateusz.recipemanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeManagerBackendApplication.class, args);
+    }
+
+}
---
+++ recipe-manager-backend/src/test/java/pl/mateusz/recipemanagerbackend/RecipeManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.recipemanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
